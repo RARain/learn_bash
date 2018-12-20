87b0bdbbb39ba5ea50d3fa580cb813a61305e9f7
chun <haozhangchun@gmail.com>
2018-12-20 23:18:22 +0800

    setup.cuda.sh无法使用wget下载cuda

--- 1.ubuntu1604/setup.cuda.sh
+++ 1.ubuntu1604/setup.cuda.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
-cuda10="https://developer.download.nvidia.com/compute/cuda/10.0/secure/Prod/local_installers/cuda_10.0.130_410.48_linux.run?6y_O1T6Z4sedG2CA37sPnmuQGHueYcOjQwMTHafUIPWpd8V0eZs5WutbmJqhU80pK_WyBLB_qvDSAeW5VAACRY2I5DFwGlGvm9diEZ9JgQyz01WF4seC7BS5QnP5v2uHxyawx6Q2GqEAxS9tQrgudIQd37wwpOmKeub_TEx-hkPiOFR0zvh6qPJ6KZ8"
-echo $cuda10
-wget -c $cuda10 -O cuda_10.0.130_410.48_linux.run
+echo $0
+
+PACKAGE=cuda_9.2.148_396.37_linux.run
+
+# 截止20181210
+# 浏览器下载runfile获得的地址
+# 由于会过期而无法使用wget
+
+find . -maxdepth 1 -name "$PACKAGE"
 if [ $? == 0 ]; then
-	sudo bash cuda_10.0.130_410.48_linux.run
+	sudo bash $PACKAGE
 	if [ $? != 0 ]; then
 		exit $?
 	fi
