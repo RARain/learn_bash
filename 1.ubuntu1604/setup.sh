cc30806d6e911b6597345795d9642d94a5ea289e
chun <haozhangchun@gmail.com>
2018-12-21 10:36:31 +0800

    1.ubuntu1604安装目录数组

--- 1.ubuntu1604/setup.sh
+++ 1.ubuntu1604/setup.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
-# 安装cuda
-bash $PWD/setup.cuda.sh
-if [ $? != 0 ] ; then
-	exit 1
-fi
+# 安装数组
+SETUP[0]=$PWD/setup.cuda.sh
+
+for i in ${SETUP[@]} ; do
+	bash $i
+	if [ $? != 0 ] ; then
+		exit 1
+	fi
+	clear
+done
 
 echo 安装结束
 exit 0
