76c0cbc60bba06409d8ba1871b7d5c57891a470a
chun <haozhangchun@gmail.com>
2018-12-21 16:32:18 +0800

    1.ubuntu1604一些提示

--- 1.ubuntu1604/setup.cuda.sh
+++ 1.ubuntu1604/setup.cuda.sh
@@ -2,11 +2,11 @@
 
 echo $0
 
-PACKAGE=cuda_9.2.148_396.37_linux.run
-
-# 截止20181210
+# 截止20181220
 # 浏览器下载runfile获得的地址
 # 由于会过期而无法使用wget
+# https://developer.nvidia.com/cuda-92-download-archive
+PACKAGE=cuda_9.2.148_396.37_linux.run
 
 find . -maxdepth 1 -name "$PACKAGE"
 if [ $? != 0 ]; then
--- 1.ubuntu1604/setup.tensorrt.sh
+++ 1.ubuntu1604/setup.tensorrt.sh
@@ -2,11 +2,11 @@
 
 echo $0
 
-PACKAGE=TensorRT-4.0.1.6.Ubuntu-16.04.4.x86_64-gnu.cuda-9.2.cudnn7.1.tar.gz
-
 # 截止20181221
 # 浏览器下载获得的地址
 # 由于会过期而无法使用wget
+# https://developer.nvidia.com/nvidia-tensorrt-4x-download
+PACKAGE=TensorRT-4.0.1.6.Ubuntu-16.04.4.x86_64-gnu.cuda-9.2.cudnn7.1.tar.gz
 
 find . -maxdepth 1 -name "$PACKAGE"
 if [ $? != 0 ]; then
